dc828b5d23374af8780773f56b3ca86d170d3272
Andres Mazo <amazobo@eafit.edu.co>
2023-07-05 06:10:55 -0500

    Creando metodo calculos
    Creando metodo para todos los calculos de dolar a otras monedas

--- Conversor/src/Conversor_moneda.java
+++ Conversor/src/Conversor_moneda.java
@@ -1,44 +1,50 @@
 import javax.swing.JOptionPane;
 
+
 public class Conversor_moneda {
-	//Esta clase se encarga de todo el calculo de las monedas
-	//la moneda que se utiliza en este codigo son dolares
+	/* Esta clase se encarga de todo el calculo de las monedas
+	la moneda que se utiliza en este codigo son dolares
 	
-	//monedas y su equivalente a dolar
-	static double peso_col = 4157.98;
-	static double peso_mex = 17.07;
-	static double euro = 0.92;
-	static double libra_ester = 0.79;
-	static double yen = 144.67;
-	static double won = 1299.05;
-	static double yuan = 7.24;
+	monedas y su equivalente a dolar
+	
+	peso_col = 4157.98;
+	peso_mex = 17.07;
+	Euro = 0.92;
+	Libra_ester = 0.79;
+	Yen = 144.67;
+	Won = 1299.05;
+	Yuan = 7.24;
+	*/
 	static String [] coin = {"Dolar","Peso Colombiano","Peso Mexicano","Euro","Libra esterlina","Yen Japones","Won Coreano","Yuan Chino"}; 
-	static double [] valores = {};
+	static double [] list_valores = {4157.98,17.07,0,79,0.92,144.67,1299.05,7.24};
 	
 	//constructor
 	public Conversor_moneda() {
 		double valor = Double.parseDouble(JOptionPane.showInputDialog(null, "ingresa la cantidad de dinero que se desea convertir:"));
-		menu_conversor();
+		menu_conversor(valor);
 	}
 	
-	public static void menu_conversor() {
+	public static void menu_conversor(double valor) {
 		String [] menu = {"De Dolares a otras monedas","De otras monedas a Dolares"};
-		Object mensaje =  JOptionPane.showInputDialog(null,"Selecciona la operación que deseas realizar","Cambios de divisa",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		String mensaje =  (String) JOptionPane.showInputDialog(null,"Selecciona la operación que deseas realizar","Cambios de divisa",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
 		if (mensaje == menu[0]) {
-			dolar_to_other();
+			dolar_to_other(valor);
+		}
+		else {
+			System.out.println("opcion no disponible");
 		}
 	}
 	
-	public static void dolar_to_other() {
+	public static void dolar_to_other(double valor) {
 		
 		String [] menu = menu_monedas();
-		Object mensaje =  JOptionPane.showInputDialog(null,"Selecciona la operación que deseas realizar","Cambios de divisa",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
-		calculos(mensaje);
+		String mensaje = (String) JOptionPane.showInputDialog(null,"Selecciona la operación que deseas realizar","Cambios de divisa",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		calculos(valor,mensaje);
 	}
 	
 	public static String[] menu_monedas() {
 		//inicializando menu
-		String [] menu = new String[6];
+		String [] menu = new String[7];
 		
 		//llenando mi vector menu
 		for (int i = 0 ; i < menu.length; i++) {
@@ -48,10 +54,11 @@ public class Conversor_moneda {
 		return menu;
 	}
 	
-	public static double calculos(Object mensaje) {
-		String [] menu = menu_monedas();
-		if(mensaje == menu[0]) {
-			
+	public static void calculos(double valor_ingresado,String mensaje) {
+		for (int i = 0; i < coin.length-1; i++) {
+			if(mensaje.toString().equalsIgnoreCase("De "+coin[0]+" a "+coin[i+1])) {
+				JOptionPane.showMessageDialog(null, "El resultado fue " + (valor_ingresado * list_valores[i]) +" "+ coin[0] +"es");
+			}
 		}
 	}
 }
