7960890509d4e13d5bd7aa9fdb33e2d47d88b91a
Andres Mazo <amazobo@eafit.edu.co>
2023-07-05 02:41:39 -0500

    Menu de monedas
    Creando metodo para seleccionar entre el cambio de dolar a otras monedas o viceversa

--- Conversor/src/Conversor_moneda.java
+++ Conversor/src/Conversor_moneda.java
@@ -5,17 +5,24 @@ public class Conversor_moneda {
 	//la moneda que se utiliza en este codigo son dolares
 	
 	//monedas y su equivalente a dolar
-	double peso_col = 4157.98;
-	double peso_mex = 17.07;
-	double euro = 0.92;
-	double libra_ester = 0.79;
-	double yen = 144.67;
-	double won = 1299.05;
-	double yuan = 7.24;
+	static double peso_col = 4157.98;
+	static double peso_mex = 17.07;
+	static double euro = 0.92;
+	static double libra_ester = 0.79;
+	static double yen = 144.67;
+	static double won = 1299.05;
+	static double yuan = 7.24;
+	static String [] monedas = {"Dolar","Peso Colombiano","Peso Mexicano","Euro","Libra esterlina","Yen Japones","Won Coreano","Yuan Chino"}; 
 	
 	//constructor
 	public Conversor_moneda() {
 		double valor = Double.parseDouble(JOptionPane.showInputDialog(null, "ingresa la cantidad de dinero que se desea convertir:"));
-		System.out.println(valor+22);
+		menu_conversor();
+	}
+	
+	public static Object menu_conversor() {
+		String [] menu = {"De Dolares a otras monedas","De otras monedas a Dolares"};
+		Object mensaje =  JOptionPane.showInputDialog(null,"Selecciona la operación que deseas realizar","Cambios de divisa",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		return mensaje;
 	}
 }
