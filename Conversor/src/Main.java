1e7778203b6d76a289319e4a1c3dcc1f1a9c421f
Andres Mazo <amazobo@eafit.edu.co>
2023-07-05 17:11:46 -0500

    Regresa_inicio
    Creando metodo para volver al menu de inicio

--- Conversor/src/Main.java
+++ Conversor/src/Main.java
@@ -5,8 +5,14 @@ public class Main {
 	public static String inicio() {
 		//Creando menu desplegable
 		String [] menu = {"Moneda","Temperatura"};
-		String seleccion = (String) JOptionPane.showInputDialog(null,"Selecciona un conversor","Conversores",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		String seleccion = (String) JOptionPane.showInputDialog(null,"Selecciona un conversor","Menu de inicio",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
 		return seleccion;
+	}
+	
+	public static String regresar_a_inicio() {
+		String [] menu = {"Si","No"};
+		String continuar = (String) JOptionPane.showInputDialog(null,"Desea volver al menu de inicio?","Continuar",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		return continuar;
 	}
 	
 	public static void main(String[] args) {
@@ -14,7 +20,7 @@ public class Main {
 		//Entrada de variables
 		if(seleccion.equalsIgnoreCase("Moneda")) {
 			Conversor_moneda divisa = new Conversor_moneda();
-			
+			regresar_a_inicio();
 		}
 	}
 }
