de84d8a968d5952635296545f48789ac655de2c6
Andres Mazo <amazobo@eafit.edu.co>
2023-07-05 01:19:57 -0500

    input grafico
    probando input grafico

--- Conversor/src/Main.java
+++ Conversor/src/Main.java
@@ -1,4 +1,9 @@
+import javax.swing.JOptionPane;
 
 public class Main {
-
+	public static void main(String[] args) {
+		String mensaje;
+		mensaje = JOptionPane.showInputDialog("Digite un mensaje");
+		JOptionPane.showMessageDialog(null,"mensaje ingresado fue: " + mensaje);
+	}
 }
