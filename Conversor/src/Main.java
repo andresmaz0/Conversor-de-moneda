302f42495483d437a229cdf61d83df09c3ee2f62
Andres Mazo <amazobo@eafit.edu.co>
2023-07-05 16:24:43 -0500

    modificando mi main
    Agregando metodo inicio

--- Conversor/src/Main.java
+++ Conversor/src/Main.java
@@ -1,14 +1,20 @@
 import javax.swing.JOptionPane;
 
 public class Main {
-	public static void main(String[] args) {
+	
+	public static void inicio() {
 		//Creando menu desplegable
 		String [] menu = {"Moneda","Temperatura"};
-		Object seleccion = JOptionPane.showInputDialog(null,"Selecciona un conversor","Conversores",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		String seleccion = (String) JOptionPane.showInputDialog(null,"Selecciona un conversor","Conversores",JOptionPane.QUESTION_MESSAGE,null,menu,menu[0]);
+		return seleccion;
+	}
 	
+	public static void main(String[] args) {
+		inicio();
 		//Entrada de variables
-		if(seleccion == "Moneda") {
+		if(seleccion "Moneda") {
 			Conversor_moneda divisa = new Conversor_moneda();
+			
 		}
 	}
 }
